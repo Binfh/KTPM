f3a5a79ba49227872f270fa9d68e0bb60a7ecba2
Binfh <binhkhong2410@gmail.com>
2024-06-04 23:00:53 +0700

    ad

---
+++ PTB1/PTB1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace PTB1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btngiai_Click(object sender, EventArgs e)
+        {
+            double a, b, ketqua;
+            a = double.Parse(txta.Text);
+            b = double.Parse(txtb.Text);
+            Giai c = new Giai(a, b);
+            ketqua = c.Solve();
+            txtkq.Text = ketqua.ToString();
+        }
+
+        
+
+        
+    }
+}
---
+++ PTB1/PTB1_Test/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PTB1;
+
+namespace PTB1_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Giai c;
+        [TestInitialize] // thiet lap du lieu dung chung cho TC
+        public void SetUp()
+        {
+            c = new Giai(10.0, 5.0);
+        }
+        [TestMethod] //TC1: a =10, b = 5, kq= -0.5
+        public void Test_PTB1_06_KhongSonBinh()
+        {
+            double expected, actual;
+            
+            expected = -0.5;
+            actual = c.Solve();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Test_NgoaiLe1_06_KhongSonBinh()
+        {
+            Giai c = new Giai(0.0, 0.0);
+
+
+            Assert.ThrowsException<ArgumentException>(() => c.Solve());
+
+        }
+        [TestMethod]
+        public void Test_NgoaiLe2_06_KhongSonBinh()
+        {
+            Giai c = new Giai(0.0, 9.3);
+            
+
+            Assert.ThrowsException<ArgumentException>(() => c.Solve());
+
+        }
+        
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\TestData.csv", "TestData#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void Test_With_Data_06_KhongSonBinh()
+        {
+            double a = double.Parse(TestContext.DataRow[0].ToString());
+            double b = double.Parse(TestContext.DataRow[1].ToString());
+
+            string expected = TestContext.DataRow[2].ToString();
+
+            Giai c = new Giai(a, b);
+
+            if ((a == 0.0 && b == 0.0) || (a == 0.0 && b != 0.0))
+            {
+                if (double.TryParse(expected, out _))
+                {
+                    string actual = c.Solve().ToString();
+                    // Báo lỗi nếu kết quả là số
+                    Assert.Fail("Expected result is a number, but an exception was not thrown.");
+                }
+                else {
+                    // Kiểm tra nếu ngoại lệ ArgumentException được ném ra
+                    Assert.ThrowsException<ArgumentException>(() => c.Solve());
+                }
+                
+            }
+            else
+            {
+                
+                string actual = c.Solve().ToString();
+                Assert.AreEqual(expected, actual);
+              
+            }
+        }
+
+    }
+}
